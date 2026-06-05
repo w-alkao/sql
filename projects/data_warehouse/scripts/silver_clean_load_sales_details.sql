a5d729481289e74861a981dd147dff84432fdb3c
w-alkao <walk.adamou@gmail.com>
2026-06-05 15:41:14 +0200

    clean and load data into crm sales details table from silver layer

---
+++ projects/data_warehouse/scripts/silver_clean_load_sales_details.sql
@@ -0,0 +1,116 @@
+-- check for unwanted space on product number
+
+SELECT sls_prd_num
+FROM bronze.crm_sales_details
+WHERE sls_prd_num != TRIM(sls_prd_num)
+
+-- check for product not in customer info
+
+SELECT *
+FROM bronze.crm_sales_details
+WHERE sls_cust_id NOT IN (
+	SELECT DISTINCT cst_id FROM silver.crm_cust_info
+);
+
+-- check for product not in product info
+
+SELECT *
+FROM bronze.crm_sales_details
+WHERE sls_prd_key NOT IN (
+	SELECT DISTINCT prd_key FROM silver.crm_prd_info
+);
+
+-- chect for invalid date 
+
+SELECT NULLIF(sls_order_dt, 0), sls_ship_dt, sls_due_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 OR sls_ship_dt <= 0 OR sls_due_dt <= 0
+
+-- check for date length
+
+SELECT sls_order_dt, sls_ship_dt, sls_due_dt
+FROM bronze.crm_sales_details
+WHERE LEN(sls_order_dt) != 8 OR LEN(sls_ship_dt) != 8 OR LEN(sls_due_dt) != 8
+
+-- check for invalid date order
+
+SELECT sls_order_dt, sls_ship_dt, sls_due_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt 
+
+-- check consistency: between sales, quantity, and price and correct them
+
+SELECT sls_sales, sls_quantity, sls_price
+FROM bronze.crm_sales_details
+WHERE
+	sls_sales != sls_quantity * sls_price
+	OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+	OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price;
+
+
+
+SELECT
+	sls_sales, sls_quantity, sls_price,
+	CASE
+		WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+			THEN sls_quantity * ABS(sls_price)
+		ELSE sls_sales
+	END AS new_sales, sls_quantity,
+	CASE
+		WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales / NULLIF(sls_quantity, 0)
+		ELSE sls_price
+	END AS new_price
+FROM bronze.crm_sales_details
+WHERE
+	sls_sales != sls_quantity * sls_price
+	OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+	OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_price
+
+
+-- Updating the data definition language of sales details table
+
+IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details (
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+-- Load the clean data into sales details table
+
+INSERT INTO silver.crm_sales_details (
+	sls_ord_num, sls_prd_key, sls_cust_id, sls_order_dt, sls_ship_dt, sls_due_dt, sls_sales, sls_quantity, sls_price
+)
+SELECT
+	sls_prd_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE
+		WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_order_dt AS NVARCHAR) AS DATE)
+	END AS sls_order_dt,
+	CAST(CAST(sls_ship_dt AS NVARCHAR) AS DATE) AS sls_ship_dt,
+	CAST(CAST(sls_due_dt AS NVARCHAR) AS DATE) AS sls_due_dt,
+	CASE
+		WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+			THEN sls_quantity * ABS(sls_price)
+		ELSE sls_sales
+	END AS sls_sales,
+	sls_quantity,
+	CASE
+		WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales / NULLIF(sls_quantity, 0)
+		ELSE sls_price
+	END AS sls_price
+FROM bronze.crm_sales_details;
+
+SELECT * FROM silver.crm_sales_details
