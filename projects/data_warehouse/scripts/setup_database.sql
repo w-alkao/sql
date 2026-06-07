d7fba05dfcbbb38065a162c542898bcd9fdc069e
w-alkao <walk.adamou@gmail.com>
2026-06-07 16:19:01 +0200

    setup database

--- projects/data_warehouse/scripts/setup_database.sql
+++ projects/data_warehouse/scripts/setup_database.sql
@@ -1,5 +1,24 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+=============================================================
+*/
+
 USE master;
 
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+
+-- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
 
 Use DataWarehouse;
